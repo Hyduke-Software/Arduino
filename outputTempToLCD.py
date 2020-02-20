d34e1b67d73175090c483fd0fd3c82dd6693ef2b
Hyduke-Software <gallimore.tj@gmail.com>
2020-02-20 21:19:41 +0000

    Create outputTempToLCD.py

---
+++ outputTempToLCD.py
@@ -0,0 +1,18 @@
+#Python script to Output Dell server temperature to an LCD panel
+#Running on Raspbery Pi, requires 
+import I2C_LCD_driver
+from time import *
+import sys
+
+mylcd = I2C_LCD_driver.lcd()
+
+counter = open("/home/pi/Desktop/counter.txt", "r")
+
+countervar = counter.readline()
+temp = open("/home/pi/Desktop/temp.txt", "r")
+tempvar = temp.read(2)
+
+mylcd.lcd_display_string("HOST: ENTER-SERVER-NAME", 1)
+mylcd.lcd_display_string(tempvar+" Degrees C", 2)
+mylcd.lcd_display_string("Number of checks:",3)
+mylcd.lcd_display_string("#" +countervar, 4)
